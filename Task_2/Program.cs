6ce7a60f67de4e3a9f9f13f6c7830359a4cf58fe
Alexey <nikos_68@mail.ru>
2024-01-16 14:51:45 +0300

    Added Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[10];
+        Random rand = new Random();
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = rand.Next(100,1000);
+            Console.WriteLine($"[{i}] = {numbers[i]}");
+        }
+        int evenCount = 0;
+        foreach (int number in numbers)
+        {
+            if ( number % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+        Console.WriteLine("Количесство чётных чисел в массиве: " + evenCount);
+    }
+}
