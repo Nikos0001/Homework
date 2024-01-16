07063a720448bcd53d4ed10a81dca499bb6afa14
Alexey <nikos_68@mail.ru>
2024-01-16 15:09:55 +0300

    Added Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 }; // Создаем исходный массив
+
+        // Переворачиваем массив
+        for (int i = 0; i < numbers.Length / 2; i++)
+        {
+            int temp = numbers[i];
+            numbers[i] = numbers[numbers.Length - 1 - i];
+            numbers[numbers.Length - 1 - i] = temp;
+        }
+
+        // Выводим результат
+        Console.WriteLine("Перевернутый массив:");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+    }
+}
